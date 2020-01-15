47260385864ad2ee4e7aaa8b02f613d620ed444b
DeepanjaliArya <59814454+DeepanjaliArya@users.noreply.github.com>
2020-01-15 18:19:51 +0530

    Update firstProgram.java
    adding two variable a and b and print their sum

--- firstProgram.java
+++ firstProgram.java
@@ -1,5 +1,8 @@
 class Message{
+    int a=10;
+    int b=20;
     public static void main(String[] args){
       System.out.println("bye");
+      System.out.println("sum of (a+b) is ::"+(a+b));  
 }
 }
